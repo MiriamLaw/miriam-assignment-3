e5441f4f0aaac2386ef5250473c3635b4ab4f90e
MiriamLaw <129810725+MiriamLaw@users.noreply.github.com>
2023-07-03 08:56:05 -0700

    Incorrect but functional

--- src/com/coderscampus/assignment3/UserService.java
+++ src/com/coderscampus/assignment3/UserService.java
@@ -3,15 +3,15 @@ package com.coderscampus.assignment3;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 //to validate, to read data from file and create user array
 
 public class UserService {
 
-	public User[] createUserArray(String username, String password, String name) {
+	private User[] users = new User[4];
 
-		User[] users = new User[4];
+	public User[] createUserArray(String username, String password, String name) {
+		users = new User[4];
 
 		for (int i = 0; i < 4; i++) {
 			User user = new User(username, password, name);
@@ -21,8 +21,6 @@ public class UserService {
 			users[i] = user;
 
 		}
-
-		System.out.println(Arrays.toString(users));
 		return users;
 	}
 
@@ -34,6 +32,7 @@ public class UserService {
 	public void readDataFromFile() {
 		try (BufferedReader fileReader = new BufferedReader(new FileReader("data.txt"))) {
 			String line;
+			int count = 0;
 			while ((line = fileReader.readLine()) != null) {
 				// process each line of the file
 				String[] userInfo = line.split(",");
@@ -43,35 +42,37 @@ public class UserService {
 				String name = userInfo[2];
 
 				User user = new User(username, password, name);
-				System.out.println(user);
+
+				users[count] = user;
+				count++;
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public boolean validateLogin(User[] users, String username, String password, String name) {
-			int loginAttempts = 0;
-			
-			for (User user : users) {
-				if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
-					System.out.println("Welcome: " + user.getName(name);
-					return true;
-				} else {
-					loginAttempts++;
-				}
-				
-				if(loginAttempts >=5) {
-					System.out.println("Too many failed login attempts, you are now locked out.");
-					return false;
-				} else {
-				
-				System.out.println("Invalid login, please try again.");
-				return false;
-	
-	
+	public boolean validateLogin(String username, String password, String name) {
+		int loginAttempts = 0;
 
-}
-}
-}
+		for (User user : users) {
+			
+			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+				System.out.println("Welcome: " + user.getName());
+				return true;
+			} else {
+				loginAttempts++;
+			}
+
+			if (loginAttempts >= 5) {
+				System.out.println("Too many failed login attempts, you are now locked out.");
+				return false;
+			} else {
+
+				System.out.println("Invalid login, please try again.");
+
+			}
+		}
+		return false;
+	}
 }
