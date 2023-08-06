388d84737f984cd0c6b461e7ca0e66693b476246
MiriamLaw <mimsical999@yahoo.com>
2023-08-06 14:31:15 -0700

    Dead code push

--- src/com/coderscampus/assignment3/UserLoginApplication.java
+++ src/com/coderscampus/assignment3/UserLoginApplication.java
@@ -5,10 +5,7 @@ public class UserLoginApplication {
 	public static void main(String[] args) {
 
 		UserService userService = new UserService();
-		boolean loginSuccessful = userService.initiateLogin();
+		userService.initiateLogin();
 
-		if (!loginSuccessful) {
-			System.out.println("Too many failed login attempts, you are now locked out.");
-		}
 	}
 }
--- src/com/coderscampus/assignment3/UserService.java
+++ src/com/coderscampus/assignment3/UserService.java
@@ -2,14 +2,13 @@ package com.coderscampus.assignment3;
 
 import java.util.Scanner;
 
-//to validate, to read data from file and create user array
-
 public class UserService {
 
 	private User[] users = new User[4];
 
 	public UserService() {
 		loadUsers();
+
 	}
 
 	private void loadUsers() {
@@ -17,10 +16,10 @@ public class UserService {
 		users = fileService.readDataFromFile();
 	}
 
-//	refactored into method 
 	public User getUserByUsernameAndPassword(String username, String password) {
+
 		for (User user : users) {
-			if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
+			if (user.getName().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
 				return user;
 			}
 		}
@@ -29,17 +28,10 @@ public class UserService {
 
 	public boolean initiateLogin() {
 
-//		let users login up to 5 incorrect attempts
-//		for each attempt:
-//		-get username and password
-//		-validate if user exists with given username and password
-//		-if user exists, login valid, show welcome message
-//		-else (no user exists), login is invalid, increast attempt counter for next iteration
-
+		int invalidAttempts = 0;
+		User loggedInUser = null;
 		try (Scanner scanner = new Scanner(System.in)) {
-			int invalidAttempts = 0;
 
-			User loggedInUser = null;
 			while (invalidAttempts < 5) {
 				System.out.println("Enter your username: ");
 				String username = scanner.nextLine();
@@ -52,58 +44,18 @@ public class UserService {
 				if (loggedInUser != null) {
 					System.out.println("Welcome " + loggedInUser.getName());
 					return true;
-				} else {
+				} else if (loggedInUser == null) {
 					System.out.println("Invalid login, please try again.");
 					invalidAttempts++;
 				}
+
 			}
 		}
+		if (invalidAttempts >= 5 && loggedInUser != null) {
+			System.out.println("Too many failed login attempts, you are now locked out.");
+		}
+
 		return false;
 	}
+
 }
-//
-//	public String[] parseText(String userInfo) {
-//		return userInfo.split(",");
-//
-//	}
-//
-//	public boolean validateLogin(String username, String password) {
-//		Scanner scanner = new Scanner(System.in);
-//		int invalidAttempts = 0;
-//		boolean loggedIn = false;
-//
-//		try {
-//			while (!loggedIn && invalidAttempts < 5) {
-//				for (User user : users) {
-//					if (user.getUsername().equalsIgnoreCase(username) && user.getPassword().equals(password)) {
-//						System.out.println("Welcome " + user.getName());
-//						loggedIn = true;
-//						break;
-//					}
-//				}
-//
-//				if (!loggedIn) {
-//					invalidAttempts++;
-//					System.out.println("Invalid login, please try again.");
-//
-//					if (invalidAttempts == null) {
-//						System.out.println("Too many failed login attempts, you are now locked out.");
-//				}
-//						return false;
-//					}
-//					}
-//
-//					System.out.println("Enter your username: ");
-//					username = scanner.nextLine();
-//
-//					System.out.println("Enter your password: ");
-//					password = scanner.nextLine();
-//
-//				}
-//			}
-//		} finally {
-//			scanner.close();
-//		}
-//		return loggedIn;
-//	}
-//}
